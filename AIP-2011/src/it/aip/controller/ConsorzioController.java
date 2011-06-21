ea56a97c9bacf1f3d4f01f7dba11bfeb727eea97
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-06-21 15:02:42 +0200

    added consorzio and biologico pages

---
+++ AIP-2011/src/it/aip/controller/ConsorzioController.java
@@ -0,0 +1,12 @@
+package it.aip.controller;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class ConsorzioController extends Controller {
+
+    @Override
+    public Navigation run() throws Exception {
+        return forward("consorzio.jsp");
+    }
+}
