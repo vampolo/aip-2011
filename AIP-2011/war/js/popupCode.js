e8fc1583957474d3bffddb08df5299aed151f029
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-06-25 16:03:50 +0200

    added popupCode

---
+++ AIP-2011/war/js/popupCode.js
@@ -0,0 +1,48 @@
+var popupStatus = 0;
+
+// Loading
+function loadPopup(){
+	if(popupStatus==0){
+		$("#backgroundPopup").css({"opacity": "0.5"});
+		$("#backgroundPopup").fadeIn("fast");
+		$("#popupNotImplemented").fadeIn("fast");
+		popupStatus = 1;
+	}
+}
+
+// Disabling
+function disablePopup(){
+	if(popupStatus==1){
+		$("#backgroundPopup").fadeOut("fast");
+		$("#popupNotImplemented").fadeOut("fast");
+		popupStatus = 0;
+	}
+}
+
+// Centering
+function centerPopup(){	
+	var windowWidth = document.documentElement.clientWidth;
+	var windowHeight = document.documentElement.clientHeight;
+	var popupHeight = $("#popupNotImplemented").height();
+	var popupWidth = $("#popupNotImplemented").width();
+	
+	$("#popupNotImplemented").css({"position": "absolute", "top": windowHeight/2-popupHeight/2, "left": windowWidth/2-popupWidth/2});
+	$("#backgroundPopup").css({"height": windowHeight});
+}
+
+// Events
+$(document).ready(function(){
+	
+	$(".notImplemented").click(function(){
+		centerPopup();
+		loadPopup();
+	});
+				
+	$("#popupNotImplementedClose").click(function(){
+		disablePopup();
+	});
+	
+	$("#backgroundPopup").click(function(){
+		disablePopup();
+	});
+});
